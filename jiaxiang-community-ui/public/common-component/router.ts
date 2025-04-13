255a97905d100d2e815da54342cf00f0c8abb5fc
syannpe <zhengruipeng3@gmail.com>
2025-04-13 13:18:17 +0800

    FrontEnd@2.10.0-20250413

--- jiaxiang-community-ui/public/common-component/router.ts
+++ jiaxiang-community-ui/public/common-component/router.ts
@@ -12,7 +12,7 @@ const router = {
     "proof": prefix + "/pages/proof/index.html",
     "community-details": prefix + "/pages/community-details/index.html",
     "community-honor": prefix + "/pages/community-honor/index.html",
-    "community-activity": prefix + "/pages/community-activity/index.html",
+    // "community-activity": prefix + "/pages/community-activity/index.html",
 
     "jiaxiang-community-map": prefix + "/files/嘉祥社区地图.svg"
 
